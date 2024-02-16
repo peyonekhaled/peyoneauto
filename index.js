dd19272b268929342c008934a550dd1d9b8bf9c3
peyonekhaled <160229246+peyonekhaled@users.noreply.github.com>
2024-02-16 14:55:21 +0400

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new Discord.Client({ checkUpdate: false });
 new userAccount(client, Discord).autoReaction({
 channel: "1107663756500140085",
 user: "1091131848383537213",
-timeout: 8000,
+timeout: 6000,
 customBotId: ['990617618899681370','824119071556763668', '294882584201003009', '894633316010066032', '1106538922697297930', '806018563307601930', '1113447941189926943', '1131684868619763843'],
 blacklistedwords: ["tokens", "auto"]
 })
